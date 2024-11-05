f0ca89e441a213b419887dbf64921853018e37b4
alireza nezami <nezamialireza1022@gmail.com>
2024-11-05 21:45:39 +0330

    install tabs

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
-<script setup>
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 </script>
 
 <template>
@@ -11,12 +12,42 @@
       <div class="w-[120px] h-[36px] bg-neutral-200"></div>
     </header>
     <main class="grid gap-2">
-      <div class="flex items-center gap-4">
+      
+  <Tabs default-value="today" class="w-[400px]">
+    <TabsList>
+      <TabsTrigger value="today">
+        Today
+      </TabsTrigger>
+      <TabsTrigger value="week">
+        This week
+      </TabsTrigger>
+      <TabsTrigger value="month">
+        This month
+      </TabsTrigger>
+      <TabsTrigger value="year">
+        This year
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent value="today">
+      today
+    </TabsContent>
+    <TabsContent value="week">
+      week
+    </TabsContent>
+    <TabsContent value="month">
+      month
+    </TabsContent>
+    <TabsContent value="year">
+      year
+    </TabsContent>
+  </Tabs>
+
+      <!-- <div class="flex items-center gap-4">
         <div v-for="(item,index) in 3" :key="index" class="w-[120px] h-[36px] bg-neutral-200"></div>
       </div>
       <section>
         <div class="w-full h-[360px] bg-neutral-200"></div>
-      </section>
+      </section> -->
     </main>
     <footer>
       <div class="flex items-center gap-4">
