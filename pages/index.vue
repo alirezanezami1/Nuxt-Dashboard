4f739e9198de1fe7f0888c56b2dcbe91ef3f8842
alireza nezami <nezamialireza1022@gmail.com>
2024-11-10 17:47:00 +0330

    change chart and create Month-generator

--- pages/index.vue
+++ pages/index.vue
@@ -20,31 +20,77 @@ const list = [
   }
 ]
 
-let categories = ref({
-  'today' : [],
-  'week' : [],
-  'month' : [],
-  'year' : []
-})
-let currentCategories = ref(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'])
 
-const options = computed(() => (
-  {
+
+let categories = ref({
+  today: [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "23:00",
+  ],
+  week: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  year: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Api',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+})
+
+let data = ref([])
+
+let currentCategory = ref("today")
+
+const options = computed(() => ({
     chart: {
         type: 'line',
         animation : {
           enabled : false
         }
     },
-    legend : {
-      enabled : false
-    },
     title: {
         text: ''
     },
     xAxis: {
       gridLineColor: 'transparent',
-        categories: currentCategories
+        categories: categories.value[currentCategory.value]
     },
     yAxis: {
        gridLineColor: 'transparent',
@@ -52,35 +98,22 @@ const options = computed(() => (
             text: ''
         }
     },
+    legend : {
+      enabled : false
+    },
     plotOptions: {
         line: {
           marker : {
             enabled : false
           },
             dataLabels: {
-                enabled: true
+                enabled: false
             },
-            enableMouseTracking: true
+            enableMouseTracking: false
         }
     },
-    series: [{
-        name: 'Line1',
-        lineWidth : '4px',
-        color : {
-          linearGradient : {},
-          stops : [
-            [0, 'yellow'],
-            [.5 , 'green'],
-            [.88 , 'green'],
-            [1 , 'orange']
-          ]
-        },
-        data: [
-            16.0, 18.2, 23.1, 25.2, 27.1, 31.7, 34.11, 36.68, 39.2, 45.2,
-            38.0, 27.9
-        ]
-    }, {
-        name: 'Line2',
+    series: [ {
+        name: 'line',
         lineWidth : '4px',
         color : {
           linearGradient : {},
@@ -90,13 +123,81 @@ const options = computed(() => (
             [1 , 'yellow']
           ]
         },
-        data: [
-            -2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.1, 16.5, 12.0, 6.5,
-            2.0, -0.9
-        ]
+        data: data.value
     }]
 }
 ))
+
+
+////////////////////////////////////////
+// function to generator current month
+
+function generateMonth() {
+  let currentDate = new Date();
+  let currentMonth = currentDate.getMonth() + 1;
+
+  let currentYear = currentDate.getFullYear()
+
+  function generateMonthDates() {
+    let monthDates = [];
+    let daysInMonth = new Date(currentYear , currentMonth, 0).getDate();
+
+
+    for(let i = 1; i <= daysInMonth ; i++) {
+      let dayString = ("0" + i).slice(-2);
+      let monthString = ("0" + currentMonth).slice(-2);
+
+      monthDates.push(monthString + "/" + dayString);
+    }
+
+    return monthDates
+
+  }
+
+  let month = generateMonthDates()
+  categories.value = ({ ...categories.value, month})
+  return month;
+
+}
+
+
+
+function generateRandomValue(number = 7) {
+  let values = []
+  for (let i = 0 ; i < number + 1; i++) {
+    values.push(Math.floor(Math.random() * 100))
+  }
+  data.value = values
+  return values
+}
+
+
+
+const setCategory = (e) => {
+  let v = e.target.innerText.toLowerCase()
+  currentCategory.value = v
+  switch (v) {
+    case 'today': 
+      generateRandomValue(24);
+      break;
+    case 'week': 
+      generateRandomValue(7);
+      break;
+    case 'month': 
+      generateRandomValue(31);
+      break;
+    case 'year': 
+      generateRandomValue(12);
+      break;
+  }
+}
+
+
+
+onMounted(() => {
+  generateMonth()
+  generateRandomValue(7)
+})
 
 </script>
 
@@ -111,8 +212,8 @@ const options = computed(() => (
     </header>
     <main class="grid gap-2">
       
-      <Tabs default-value="Today" >
-        <TabsList class="w-[400px]">
+      <Tabs default-value="Today" @click="setCategory">
+        <TabsList class="max-w-[400px]">
           <TabsTrigger v-for="item , index in list" :key="index" :value="item.title">
             {{ item.title }}
           </TabsTrigger>
